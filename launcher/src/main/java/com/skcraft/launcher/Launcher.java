550daedbe4c0df05d77f0f1e7782570ee81e32b9
Dev_Codec <joshricker410@gmail.com>
2023-01-27 20:55:08 -0600

    Move to new fork of SKCraft.
    1.02 AI

--- launcher/src/main/java/com/skcraft/launcher/Launcher.java
+++ launcher/src/main/java/com/skcraft/launcher/Launcher.java
@@ -113,16 +113,16 @@ public final class Launcher {
      */
     public void setDefaultConfig() {
         double configMax = config.getMaxMemory() / 1024.0;
-        double suggestedMax = 2;
+        double suggestedMax = 8;
         double available = Double.MAX_VALUE;
 
         try {
             OperatingSystemMXBean bean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
             available = bean.getTotalPhysicalMemorySize() / 1024.0 / 1024.0 / 1024.0;
-            if (available <= 6) {
-                suggestedMax = available * 0.48;
+            if (available <= 8) {
+                suggestedMax = available * 0.75;
             } else {
-                suggestedMax = 4;
+                suggestedMax = 8;
             }
         } catch (Exception ignored) {
         }
